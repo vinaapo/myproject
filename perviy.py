2de36b5c55035bf2eec59871907724d640606e8b
user <130287380+vinaapo@users.noreply.github.com>
2023-04-09 16:52:55 +0500

    Create perviy.py

---
+++ perviy.py
@@ -0,0 +1,11 @@
+a = input('Как тебя зовут?)
+print('Привет, ', a)
+b = int(input('Сколько тебе лет?')
+print(ОГО! А мне всего 5')
+c = list(map(int. input('Назови свои любимые цифры:').split()))
+print(*c')
+d = int(input('Тебе нравится GetHub?)):
+    if d == 'Да and d == 'да':
+        print('Ок)
+    else
+        print'Пон')
